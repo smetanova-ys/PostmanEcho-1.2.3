c40ec401ba7d644041c4e3e4513c9b411f8f4de1
smetanova-ys <smetanova.ys@gmail.com>
2022-03-25 12:18:41 +0100

    Test was corrected

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -18,7 +18,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("super data"))
+                .body("data", equalTo("some data"))
         ;
     }
 }
